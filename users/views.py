8b512d1fda4dc3975160991a561d049f146afc45
jnhallquist <jnhallquist@gmail.com>
2020-05-07 20:36:11 -0700

    Create User viewset

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import HeroSerializer
+from .models import Hero
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('email')
+    serializer_class = UserSerializer
